ed8b198a6f1c26c17d7ffd7fee1526ba0f7d35ba
Prakhar Singh <prakharsingh11111@gmail.com>
2020-09-21 02:23:45 +0530

    canvas

---
+++ build/precache-manifest.4f6f592db42e3f94a0b0eac14090884c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aaa0fe57fbae3e657f5924db96f71b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9affcbfd2d824d300362",
+    "url": "/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "8bf9c01b18acb1ad0727",
+    "url": "/static/css/main.44388552.chunk.css"
+  },
+  {
+    "revision": "9affcbfd2d824d300362",
+    "url": "/static/js/2.f2bc5e73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2bc5e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf9c01b18acb1ad0727",
+    "url": "/static/js/main.7ad307d5.chunk.js"
+  },
+  {
+    "revision": "ca14e5dd693ca3264556",
+    "url": "/static/js/runtime-main.6cf1399c.js"
+  }
+]);
